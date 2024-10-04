6c3379e40964ae0d08e0eb3cdbdb2b027c29290f
Lena Ray <sonalibhattacharjeeca@gmail.com>
2024-10-03 22:08:28 -0700

    web player added

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -4,12 +4,16 @@ import { useRouter } from 'next/router';
 import { auth, db } from '../firebaseClient';
 import { setDoc, doc, getDoc } from '@firebase/firestore';
 import axios from 'axios';
+import DatePicker from 'react-datepicker';
+import Slider from '@mui/material/Slider';
+import { MenuItem, Select, FormControl, InputLabel } from '@mui/material';
+import 'react-datepicker/dist/react-datepicker.css';
 import { loadGetInitialProps } from 'next/dist/shared/lib/utils';
 import styles from '../styles/MainPage.module.css';
 import AuthProvider from "./AuthProvider";
 import ProfileButton from '../components/ProfileButton';
 import SpotifyMusicPlayer from '../components/WebPlayer';
-import { getToken } from 'next-auth/jwt';
+import MenuIcon from '@mui/icons-material/Menu';
 
 const MainScreen = () => {
     const [showPopup, setShowPopup] = useState(false);
@@ -18,6 +22,10 @@ const MainScreen = () => {
     const [followedArtists, setFollowedArtists] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [accessToken, setAccessToken] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [priceRange, setPriceRange] = useState([0, 100]);
+    const [selectedArtist, setSelectedArtist] = useState('');
+    const [showFilters, setShowFilters] = useState(false);
     const router = useRouter();
 
     const SPOTIFY_CLIENT_ID = process.env.NEXT_PUBLIC_SPOTIFY_CLIENT_ID;
@@ -27,28 +35,43 @@ const MainScreen = () => {
     const AUTH_URL = `https://accounts.spotify.com/authorize?client_id=${SPOTIFY_CLIENT_ID}&response_type=code&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=user-follow-read`;
 
     useEffect(() => {
-        const token = localStorage.getItem('spotify_access_token');
-        const tokenExpiry = Number(localStorage.getItem('spotify_token_expiry'));
-        const refreshToken = localStorage.getItem('spotify_refresh_token');
+        const storedArtists = localStorage.getItem('followedArtists');
+        const storedConcerts = localStorage.getItem('concerts');
 
-        const isTokenExpired = tokenExpiry && new Date().getTime() > tokenExpiry;
+        // fetch data from local storage if available
+        if (storedArtists) {
+            console.log("stored artists");
+            setFollowedArtists(JSON.parse(storedArtists));
+        }
 
-        if (token && !isTokenExpired) {
-            setAccessToken(token);
-            // console.log("not expired");
-            fetchFollowedArtists(token).then(() => {
-                if (followedArtists.length > 0) {
-                    fetchConcertsForFollowed(followedArtists);
-                }
-                setIsLoading(false);
-            });
-        } else if (refreshToken) {
-            fetchNewAccessToken(refreshToken);
+        if (storedConcerts) {
+            console.log("stored concerts");
+            setConcerts(JSON.parse(storedConcerts));
             setIsLoading(false);
         } else {
-            setShowPopup(true);
-            fetchPopularConcerts();
-            setIsLoading(false);
+            const token = localStorage.getItem('spotify_access_token');
+            const tokenExpiry = Number(localStorage.getItem('spotify_token_expiry'));
+            const refreshToken = localStorage.getItem('spotify_refresh_token');
+
+            const isTokenExpired = tokenExpiry && new Date().getTime() > tokenExpiry;
+
+            if (token && !isTokenExpired) {
+                setAccessToken(token);
+                // console.log("not expired");
+                fetchFollowedArtists(token).then(() => {
+                    if (followedArtists.length > 0) {
+                        fetchConcertsForFollowed(followedArtists);
+                    }
+                    setIsLoading(false);
+                });
+            } else if (refreshToken) {
+                fetchNewAccessToken(refreshToken);
+                setIsLoading(false);
+            } else {
+                setShowPopup(true);
+                fetchPopularConcerts();
+                setIsLoading(false);
+            }
         }
 
         const unsubscribe = auth.onAuthStateChanged(user => {
@@ -95,6 +118,7 @@ const MainScreen = () => {
 
     const fetchConcertsForFollowed = async (artists) => {
         setIsLoading(true);
+
         const userId = auth.currentUser ? auth.currentUser.uid : null;
         if (!userId) {
             console.error('No user is currently logged in.');
@@ -136,7 +160,8 @@ const MainScreen = () => {
 
                 if (concerts.length > 0) {
                     setConcerts(concerts); // Set all found concerts
-                    await saveConcerts(userId, concerts);
+                    localStorage.setItem('concerts', JSON.stringify(concerts));
+                    // await saveConcerts(userId, concerts);
                 } else {
                     console.log("No events found for the followed artists.");
                     setConcerts([]); // Set concerts to an empty array if no events found
@@ -212,9 +237,37 @@ const MainScreen = () => {
         setSearchQuery(e.target.value);
     }
 
-    const filteredConcerts = concerts.filter((concert) =>
-        concert.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    }
+
+    const handlePriceChange = (event, newValue) => {
+        setPriceRange(newValue);
+    }
+
+    const handleArtistChange = (event) => {
+        setSelectedArtist(event.target.value);
+    }
+
+    const handleFilterToggle = () => {
+        setShowFilters(prev => !prev);
+        console.log(showFilters);
+    }
+
+    const handleApplyFilters = () => {
+        setShowFilters(false);
+    }
+
+    const filteredConcerts = concerts.filter((concert) => {
+        const concertDate = new Date(concert.dates.start.localDate);
+        const isWithinDate = selectedDate ? concertDate.toDateString() === selectedDate.toDateString() : true;
+        const isWithinPrice = concert.priceRanges ? concert.priceRanges[0].min <= priceRange[1] && concert.priceRanges[0].max >= priceRange[0] : true;
+        return (
+            concert.name.toLowerCase().includes(searchQuery.toLowerCase()) &&
+            isWithinDate &&
+            isWithinPrice
+        );
+    });
 
     const fetchFollowedArtists = async (accessToken) => {
         try {
@@ -226,15 +279,15 @@ const MainScreen = () => {
 
             const followedArtists = response.data.artists.items;
             setFollowedArtists(followedArtists);
-            // console.log('Followed Artists:', followedArtists);
+            localStorage.setItem('followedArtists', JSON.stringify(followedArtists));
 
-            const userId = auth.currentUser ? auth.currentUser.uid : null;
+            // const userId = auth.currentUser ? auth.currentUser.uid : null;
 
-            if (userId) {
-                await saveFollowedArtists(userId, followedArtists); // Save to Firestore
-            } else {
-                console.error('No user is currently logged in.');
-            }
+            // if (userId) {
+            //     await saveFollowedArtists(userId, followedArtists); // Save to Firestore
+            // } else {
+            //     console.error('No user is currently logged in.');
+            // }
         } catch (error) {
             console.error('Error fetching followed artists:', error);
         }
@@ -269,21 +322,28 @@ const MainScreen = () => {
                     </div>
             </Popup>
 
-            {/* <div className={styles.sideContent}>
-                {accessToken && <SpotifyMusicPlayer accessToken={accessToken} followedArtists={followedArtists} /> }
-                {followedArtists.length > 0 && (
-                    <div className={styles.artistListings}>
-                        <h3>Your Followed Artists:</h3>
-                        {followedArtists.map((artist: any) => (
-                            <div key={artist.id} className={styles.artistItem}>
-                                <h4>{artist.name}</h4>
-                            </div>
-                        ))}
-                    </div>
-                )}
-            </div> */}
+            <div className={styles.leftSection}>
+                <p className={styles.logo}>setlist</p>
 
-            <div className={styles.header}>
+                <div className={styles.spotifyPlayer}>
+                    {accessToken && (
+                        <iframe
+                            src={`https://open.spotify.com/embed/artist/${followedArtists[0]?.id}`}
+                            width="300"
+                            height="380"
+                            frameBorder="0"
+                            allow="encrypted-media"
+                            allowTransparency="true"
+                        ></iframe>
+                    )}
+                </div>
+            </div>
+
+            <div className={styles.filterToggle} onClick={handleFilterToggle}>
+                <MenuIcon />
+            </div>
+
+            <div className={styles.centerSection}>
                 <input
                     type="text"
                     placeholder="Search concerts"
@@ -291,42 +351,62 @@ const MainScreen = () => {
                     onChange={handleSearch}
                     className={styles.searchBar}
                 />
-                <div className={styles.relinkButtonWrapper}>
-                    <button onClick={linkSpotify} className={styles.relinkButton}>
-                        Relink Spotify
-                    </button>
-                    <ProfileButton />
-                </div>
+                {isLoading && <div className={styles.loading}>Loading...</div>}
+                {!isLoading && (
+                    <div className={styles.concertListings}>
+                    {filteredConcerts.length > 0 ? (
+                        filteredConcerts.map((concert) => (
+                        <div key={concert.id} className={styles.concertItem}>
+                            <h3 className="concertName">{concert.name}</h3>
+                            <p className="date">{new Date(concert.dates.start.localDate).toLocaleDateString()}</p>
+                            <p className="venue">{concert._embedded.venues[0].name}</p>
+                            <a href={concert.url} target="_blank" rel="noopener noreferrer">View Tickets</a>
+                        </div>
+                        ))
+                    ) : (
+                        <p>No concerts found</p>
+                    )}
+                    </div>
+                )}
             </div>
 
-            {isLoading && <div className={styles.loading}>Loading...</div>}
-            {!isLoading && (
-                <div className={styles.concertListings}>
-                {filteredConcerts.length > 0 ? (
-                    filteredConcerts.map((concert) => (
-                    <div key={concert.id} className={styles.concertItem}>
-                        <h3 className="concertName">{concert.name}</h3>
-                        <p className="date">{new Date(concert.dates.start.localDate).toLocaleDateString()}</p>
-                        <p className="venue">{concert._embedded.venues[0].name}</p>
-                        <a href={concert.url} target="_blank" rel="noopener noreferrer">View Tickets</a>
+            <div className={styles.relinkButtonWrapper}>
+                <button onClick={linkSpotify} className={styles.relinkButton}>
+                    Relink Spotify
+                </button>
+                <ProfileButton />
+            </div>
+
+            {showFilters && (
+                <div className={styles.filterOptions}>
+                    <div className={styles.filterGroup}>
+                        <DatePicker selected={selectedDate} onChange={handleDateChange} />
                     </div>
-                    ))
-                ) : (
-                    <p>No concerts found</p>
-                )}
+                    {/* <div className={styles.filterGroup}>
+                        <label>Price Range:</label>
+                        <Slider
+                            value={priceRange}
+                            onChange={handlePriceChange}
+                            valueLabelDisplay="auto"
+                            min={0}
+                            max={100}
+                        />
+                    </div> */}
+                    <div className={styles.filterGroup}>
+                        <FormControl fullWidth>
+                            <InputLabel>Artist</InputLabel>
+                            <Select value={selectedArtist} onChange={handleArtistChange}>
+                                {/* Populate this Select with artist options */}
+                                {followedArtists.map(artist => (
+                                    <MenuItem key={artist.id} value={artist.name}>{artist.name}</MenuItem>
+                                ))}
+                            </Select>
+                        </FormControl>
+                    </div>
+                    <button onClick={handleApplyFilters}>Apply Filters</button>
                 </div>
             )}
 
-            {followedArtists.length > 0 && (
-                <div className={styles.artistListings}>
-                    <h3>Your Followed Artists:</h3>
-                    {followedArtists.map((artist: any) => (
-                        <div key={artist.id} className={styles.artistItem}>
-                            <h4>{artist.name}</h4>
-                        </div>
-                    ))}
-                </div>
-            )}
         </div>
     )
 };
